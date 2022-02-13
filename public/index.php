da7848a9027cae9db160bfd95aec48c135752a1a
Idris Aweda Zubair <zubairidrisaweda@gmail.com>
2022-02-13 14:04:25 +0100

    Environment config

--- public/index.php
+++ public/index.php
@@ -1,5 +1,12 @@
 <?php
 
-require_once __DIR__ . '../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-echo "Hello there!";
+use App\Environment;
+
+$environment = new Environment('');
+$environment->load();
+
+$secret_something = getenv('XXX_KEY');
+
+var_dump($secret_something);
